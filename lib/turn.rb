2b01cd6dcef8c1c9cee9557c8740eb1b3991769d
Ronnie Thomas <askmrrthomas@gmail.com>
2019-03-24 17:50:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def display_board(board)
-  puts "  X | X  | X "
+  puts "  X | X | X "
   puts "-----------"
-  puts "  X | X  | X "
+  puts "  X | X | X "
   puts "-----------"
-  puts "  X | X  | X "
+  puts "  X | X | X "
 end
